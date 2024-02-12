50bd9d049552b6566f8dd66f48758637d0dce740
IGSparkew <alex.robbrecht@sfr.fr>
2024-02-12 23:55:42 +0100

    try to add a newer way to make electron app

---
+++ electron-correction/src/renderer/src/App.jsx
@@ -0,0 +1,9 @@
+
+export function App() {
+  return (
+    <div>
+      <h1>Hello vite + preact</h1>
+    </div>
+  )
+}
+
---
+++ electron-correction/src/renderer/src/main.jsx
@@ -0,0 +1,6 @@
+import './assets/main.css'
+import App from './App'
+import { render } from 'preact'
+
+
+render(<App />, document.getElementById('app'));
