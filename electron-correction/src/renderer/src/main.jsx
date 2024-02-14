7ce2d3067613da925d71702094839ac11d209c05
IGSparkew <40170910+IGSparkew@users.noreply.github.com>
2024-02-14 13:01:20 +0100

    Add comment correction (#12)

--- electron-correction/src/renderer/src/main.jsx
+++ electron-correction/src/renderer/src/main.jsx
@@ -1,5 +1,5 @@
 import { App } from './App.jsx';
 import { render } from 'preact';
 
-
+// call the App component to render it in the DOM
 render(<App />, document.getElementById('app'));
--- serviceWorker-correction/src/components/HistoryDate.jsx
+++ serviceWorker-correction/src/components/HistoryDate.jsx
@@ -3,12 +3,14 @@ import { useCallback } from "preact/hooks";
 export function HistoryDate({dates}) {
 
     const removeDate = useCallback((key) => {
+        // call service worker to remove date from index db
         navigator.serviceWorker.controller.postMessage({action: 'delete', key: key});
     },[]);
 
     return(
         <div class="container">
             <h2 class="title">Dates dans index db:</h2>
+            {/* loop to show all dates in index db from props */}
             <ul class="date-list">
                 {dates.map((date, index) => 
                 (
